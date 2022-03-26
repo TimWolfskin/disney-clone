31f82288450466b26faf73027c74c78cbf4001eb
Timur Aliev <timwolfskin013.gmail.com>
2022-03-26 19:06:04 +0300

    get started the project

---
+++ src/components/Login.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Login = () => {
+    return (
+        <Container>
+            <Content>
+                ffffffffff
+            </Content>
+        </Container>
+    );
+};
+
+const Container = styled.section`
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    height: 100vh;
+`;
+
+const Content = styled.div`
+
+`
+
+export default Login
