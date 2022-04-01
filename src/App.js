16b4c4ff46d57c452879d78bb9afcb34bbe8deb8
Timur Aliev <timwolfskin013.gmail.com>
2022-04-01 10:11:17 +0300

    added Detail component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Login from "./components/Login";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import './App.css';
+import Detail from "./components/Detail";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
         </Routes>
       </BrowserRouter>
     </div>
